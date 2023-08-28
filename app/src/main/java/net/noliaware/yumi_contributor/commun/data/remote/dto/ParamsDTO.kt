cbf24938342bb577c744cef07adb6f0d9cd9fa84
Bayram M'KININI <bayram_mkinini.external@discovery.com>
2023-08-28 20:52:53 +0100

    FEAT: Implement progard

--- app/src/main/java/net/noliaware/yumi_contributor/commun/data/remote/dto/ParamsDTO.kt
+++ app/src/main/java/net/noliaware/yumi_contributor/commun/data/remote/dto/ParamsDTO.kt
@@ -1,7 +1,9 @@
 package net.noliaware.yumi_contributor.commun.data.remote.dto
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class ParamsDTO(
     @Json(name = "level")
     val level: String
