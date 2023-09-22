a841f962a9b032e1e5d989d3a7f612f9eff0b3fb
Bayram M'KININI <bayram_mkinini.external@discovery.com>
2023-09-22 21:47:17 +0100

    FEAT: Implement navigation

--- app/src/main/java/net/noliaware/yumi_contributor/feature_account/domain/model/VoucherCodeData.kt
+++ app/src/main/java/net/noliaware/yumi_contributor/feature_account/domain/model/VoucherCodeData.kt
@@ -1,8 +1,10 @@
 package net.noliaware.yumi_contributor.feature_account.domain.model
 
 import android.os.Parcelable
+import androidx.annotation.Keep
 import kotlinx.parcelize.Parcelize
 
+@Keep
 @Parcelize
 data class VoucherCodeData(
     val voucherId: String?,
